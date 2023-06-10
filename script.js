56ecf95d386850f1cd0dd7afd8986b1f2a57a0aa
Nikita Yadav <119739664+Nikita2704@users.noreply.github.com>
2023-06-11 01:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+team = players; 
+team1 = players.copy();
+
+cap1 = person.copy();
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
